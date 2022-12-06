1062f12613bcd7b44ab3bff8a11333b467dae8c8
Nick Escalona <nicholas.escalona@gmail.com>
2022-12-05 23:04:44 -0600

    day6

---
+++ day6/part1.py
@@ -0,0 +1,20 @@
+import collections
+from itertools import islice
+import fileinput
+
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+line = next(fileinput.input())
+for i, char in enumerate(sliding_window(line, 4)):
+    if len(set(char)) == 4:
+        break
+
+print(i + 4)
